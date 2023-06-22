27d654857bde88bc048b93b3bbcacd42846c8412
rajan0323 <133194869+rajan0323@users.noreply.github.com>
2023-06-22 21:20:34 +0530

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,31 @@
+public class Main {
+    public static void main(String[] args) {
+        paterrn(5);
+    }
+
+    public static void paterrn(int n) {
+        for (int i = 0; i <= 2*n; i++) {
+            int noofrow=i>n?2*n-i:i;
+
+            for (int j = 0; j <= noofrow; j++) {
+                System.out.print("*");
+            }
+            int SPACE=2*(n-noofrow);
+            for(int s=0;s<=SPACE;s++){
+                System.out.print(" ");
+            }
+            for (int j = 0; j <= noofrow; j++) {
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+    }
+}
+
+
+
+
+
+
+
+
