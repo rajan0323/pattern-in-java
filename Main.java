5891c9a89eef6edda599dcb59ca4ecd9351bde95
rajan0323 <133194869+rajan0323@users.noreply.github.com>
2023-06-22 22:20:58 +0530

    Update Main.java

--- Main.java
+++ Main.java
@@ -22,28 +22,7 @@ public class Main {
     }
 }
 
-public class Main {
-    public static void main(String[] args) {
-        patttern(5);
-    }
 
-    public static void patttern(int n) {
-        for (int i = 0; i <= n; i++) {
-            int space = n - i;
-            for (int s = 0; s <= space; s++) {
-                System.out.print("  ");
-            }
-                for (int j = i; j >= 1; j--) {
-                    System.out.print(j + " ");
-                }
-                for (int j = 2; j <= i; j++) {
-                    System.out.print(j + " ");
-
-                }
-                System.out.println();
-            }
-        }
-    }
 
 
 
